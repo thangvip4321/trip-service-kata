d9c867f9ee7c2311632d477e027cf802d2b8a664
hardcoresummer <ducthangnguyen0609@gmail.com>
2024-01-01 02:11:23 +0700

    Refactor TripServiceTest class

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -6,58 +6,57 @@ using TripServiceKata.Trip;
 using TripServiceKata.User;
 namespace TripServiceKata.Tests
 {
-    public class TestableTripService : TripService
-    {
-        private User.User loggedInUser = null;
-        protected override User.User GetLoggedUser()
-        {
-            return loggedInUser;
-        }
-        public TestableTripService(TripDAO dao, User.User loggedInUser) :base(dao){
-            this.loggedInUser = loggedInUser;
-        }
-    }
+
     public class TripServiceTest
     {
+        public class TestableTripService : TripService
+        {
+            protected override User.User GetLoggedUser()
+            {
+                return loggedInUser;
+            }
+            public TestableTripService(TripDAO dao) : base(dao) { }
+        }
         // public User.User LOGGED_IN_USER = new User.User();
         private static readonly User.User GUEST = null;
         private static readonly User.User ANY_USER = new User.User();
+        private static readonly User.User REGISTERED_USER = new User.User();
         private TripDAO mockDao;
+        private static User.User loggedInUser;
+        private TripService tripService;
         [SetUp]
         public void SetUp()
         {
+            loggedInUser = REGISTERED_USER;
             mockDao = Substitute.For<TripDAO>();
+            tripService = new TestableTripService(mockDao);
+
         }
         [Test]
         public void TripService_WhenUserIsNotLoggedIn_ShouldThrowAnException()
         {
-            User.User GUEST = null;
-            var service = new TestableTripService(mockDao,GUEST);
-            Assert.Throws<UserNotLoggedInException>(() => service.GetTripsByUser(null));
-            Assert.Throws<UserNotLoggedInException>(() => service.GetTripsByUser(ANY_USER));
+            loggedInUser = GUEST;
+            Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(null));
+            Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(ANY_USER));
         }
         [Test]
         public void TripService_Return_No_Trip_If_User_Are_Not_Friend()
         {
-            var loggedInUser = new User.User();
-            var service = new TestableTripService(mockDao,loggedInUser);
             var stranger = new User.User();
             stranger.AddTrip(new Trip.Trip());
-            var trips = service.GetTripsByUser(stranger);
+            var trips = tripService.GetTripsByUser(stranger);
             Assert.IsEmpty(trips);
         }
 
         [Test]
-        public void TripService_Return_Trip_If_User_Are_Friend(){
-            var loggedInUser = new User.User();
+        public void TripService_Return_Trip_If_User_Are_Friend()
+        {
             var friend = new User.User();
             friend.AddFriend(loggedInUser);
             friend.AddTrip(new Trip.Trip());
-            var service = new TestableTripService(mockDao,loggedInUser);
             mockDao.FindTripsByUser(friend).Returns(friend.Trips());
-            
-            var trips = service.GetTripsByUser(friend);
-            Assert.AreEqual(1,trips.Count);
+            var trips = tripService.GetTripsByUser(friend);
+            Assert.AreEqual(1, trips.Count);
         }
     }
 }
